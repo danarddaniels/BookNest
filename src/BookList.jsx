721873b818434c25cf7689af7cfb0a3ee75dfdf7
Danard Daniels <danarddaniels@gmail.com>
2026-04-28 18:03:55 -0400

    Debug over

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -238,7 +238,6 @@ function BookList() {
 
 	return (
 		<div className='booklist-page'>
-			
 			<section className='filter'>
 				<input
 					placeholder='Search title...'
@@ -269,9 +268,7 @@ function BookList() {
 							key={book._id || book.id}
 							className='book'
 							style={{ backgroundImage: `url(${book.cover})` }}
-							onClick={() => handleBookClick(book), (e)=>{
-								console.log(e.target)
-							}}
+							onClick={() => handleBookClick(book)}
 						></li>
 					))}
 
